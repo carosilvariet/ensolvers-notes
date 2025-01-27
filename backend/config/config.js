fd5e01e9bc69cd0479f9a2541e6529ffdd935cd7
Tu Nombre <140862540+carosilvariet@users.noreply.github.com>
2025-01-27 01:13:40 -0300

    Fix database config for Render deployment

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
     username: process.env.DB_USER || "postgres",
-    password: `${process.env.DB_PASS}` || "Michigan2388",
+    password: process.env.DB_PASS || "Michigan2388",
     database: process.env.DB_NAME || "ensolvers_notes",
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 5432,
-    dialect: "postgres"
-  }
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
-
